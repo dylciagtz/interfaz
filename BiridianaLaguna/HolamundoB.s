cf4808824e647b1b63a92e1ebc79d0c7a61b6695
Laguna Hernandez Biridiana <biridiana.laguna17@tectijuana.edu.mx>
2017-02-03 21:43:24 -0800

    Prac.HolaMundo

---
+++ BiridianaLaguna/HolamundoB.s
@@ -0,0 +1,24 @@
+
+# Title: Practica 1 Hola mundo en ensamblador		Filename: Pract1HolaMundoJC.s
+# Author: Laguna Hernandez Biridiana 			Date: Jueves 2 / Feb / 2017
+# Description: Hola Mundo en ARM
+# Input: -
+# Output: hello world
+
+.text           
+##### Code segment ############################################################ 
+.global _start
+_start:
+    mov r0, #1
+    ldr r1, =message
+    ldr r2, =len
+    mov r7, #4
+    swi 0 #avisando al sistema
+
+    mov r7, #1
+    swi 0 #re avisando al sistema
+####### Data segment ##########################################################
+.data
+message:
+    .asciz "hello world\n" #mensaje
+len = .-message
