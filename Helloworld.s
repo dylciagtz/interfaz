4db4b5699c0d6a703a96de6bf6cca0b016433d16
Joel Vivanco <vivanco1495@gmail.com>
2017-02-07 18:17:26 -0800

    Add files via upload

---
+++ Helloworld.s
@@ -0,0 +1,19 @@
+@ hello.s - "Hello World!" program
+
+.data                @ store string and length in 2 variables
+msg:
+    .ascii  "Hello World!\n"
+len = . - msg
+
+.globl   _start
+.section .text
+_start:
+    mov r0, $1
+    ldr r1, =msg       @ pointer string to be printed in r1
+    ldr r2, =len       @ length of string in r2
+    mov r7, $4         @ prepare to output to console
+    swi $0             @ wake kernel
+
+    mov r0, $0         @ set the return value to 0
+    mov r7, $1         @ prepare to exit
+    svc $0             @ wake kernel
